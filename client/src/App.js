6e8c23cd67cb20790a234124c5cf0380d9554eac
Brendan Schatz <brendanschatz97@gmail.com>
2019-11-15 12:59:10 -0500

    got my edit and delete made

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import BubblePage from './components/BubblePage'
 import ProtectedRoute from "./components/ProtectedRoutes";
 import Login from "./components/Login";
@@ -8,6 +8,9 @@ import "./styles.scss";
 function App() {
   return (
     <Router>
+      <Link to='/'>
+        <button onClick={() => localStorage.removeItem('token')}>Sign Out</button>
+      </Link>
       <div className="App">
         <Route exact path="/" component={Login} />
         <ProtectedRoute exact path='/bubbles' component={BubblePage} />
